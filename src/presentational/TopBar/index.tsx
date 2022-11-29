c3a7b4fdcc3e37255b1bb90a34802fb50fb4cfea
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-11-29 16:12:55 +0000

    a

--- src/presentational/TopBar/index.tsx
+++ src/presentational/TopBar/index.tsx
@@ -1,11 +1,13 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, useTheme, useMediaQuery } from "@mui/material";
 import { Colors } from "../../constants/pallette";
 import { i18n } from "../../translations/i18n";
 
 const TopBar = () => {
+  const Theme = useTheme()
+  const mobile = useMediaQuery(Theme.breakpoints.down("sm"))
   return (
     <Container>
-      <Grid container justifyContent="flex-end" columnGap={2}>
+      <Grid container justifyContent={mobile ? "center" : "flex-end"} columnGap={2}>
         <Grid item><Typography color={Colors.tealc}>{i18n.t("topBar.memory")}</Typography></Grid>
         <Grid item><Typography color={Colors.tealc}>{i18n.t("topBar.hope")}</Typography></Grid>
         <Grid item><Typography color={Colors.tealc}>{i18n.t("topBar.audacity")}</Typography></Grid>
