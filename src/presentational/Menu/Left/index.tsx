95c509e4e5d521da0480f547b634e75a56e4cb17
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-12-23 17:10:27 +0000

    a

--- src/presentational/Menu/Left/index.tsx
+++ src/presentational/Menu/Left/index.tsx
@@ -1,4 +1,4 @@
-import { CardMedia } from "@mui/material";
+import { CardMedia, useTheme, useMediaQuery, Box } from "@mui/material";
 import logo from "../../../assets/images/logoSite.svg";
 import { useNavigate } from "react-router-dom";
 import { ROUTE_PATHS } from "../../../constants/routes";
@@ -9,15 +9,20 @@ interface Props {
 
 const Left = ({ height }: Props) => {
   const navigate = useNavigate();
+  const Theme = useTheme()
+  const mobile = useMediaQuery(Theme.breakpoints.down("md"))
   return (
-    <CardMedia
-      component='img'
-      height={height || "55"}
-      onClick={() => navigate(ROUTE_PATHS.HOME)}
-      image={logo}
-      style={{ cursor: "pointer" }}
-      alt='logo'
-    ></CardMedia>
+    <Box style={{ height: mobile ? "44px" : "55px" }}>
+      <CardMedia
+        component='img'
+        height="100%"
+        sx={{ objectFit: "contain" }}
+        onClick={() => navigate(ROUTE_PATHS.HOME)}
+        image={logo}
+        style={{ cursor: "pointer" }}
+        alt='logo'
+      ></CardMedia>
+    </Box>
   );
 };
 
