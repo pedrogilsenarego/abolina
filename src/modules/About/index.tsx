5681005a85bfb6b953d239c65cc1e8e2246da5d9
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-01-13 09:58:10 +0000

    a

--- src/modules/About/index.tsx
+++ src/modules/About/index.tsx
@@ -1,35 +1,44 @@
 import * as GStyled from "../../styles";
 import { i18n } from "../../translations/i18n";
-import { Typography, Container, Box, Grid, useTheme, useMediaQuery } from "@mui/material";
+import {
+  Typography,
+  Container,
+  Box,
+  Grid,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import sobreOndas from "../../assets/images/sobreOndas.svg";
 import CardMine from "./CardMine";
 import { Colors } from "../../constants/pallette";
 
 const About = () => {
-
-  const Theme = useTheme()
-  const mobile = useMediaQuery(Theme.breakpoints.down("sm"))
+  const Theme = useTheme();
+  const mobile = useMediaQuery(Theme.breakpoints.down("sm"));
 
   return (
     <Box style={{ position: "relative", paddingBottom: "15vh" }}>
       <Container>
         <Box display='flex' justifyContent='start'>
-          <Typography color={Colors.tealc} fontSize="28px" fontWeight={800}>{i18n.t("menuBar.about")}</Typography>
+          <Typography color={Colors.tealc} fontSize='28px' fontWeight={800}>
+            {i18n.t("menuBar.about")}
+          </Typography>
         </Box>
-        <Typography
-
-          style={{ marginTop: "20px", whiteSpace: "pre-line" }}
-        >
+        <Typography align="justify" style={{ marginTop: "20px", whiteSpace: "pre-line" }}>
           {i18n.t("modules.about.mainText")}
         </Typography>
-        <Container maxWidth='lg' style={{ paddingLeft: mobile ? "0px" : "80px", paddingRight: mobile ? "0px" : "80px" }}>
-
+        <Container
+          maxWidth='lg'
+          style={{
+            paddingLeft: mobile ? "0px" : "80px",
+            paddingRight: mobile ? "0px" : "80px",
+          }}
+        >
           <Grid
             container
             columnSpacing={8}
             alignItems='top'
             rowSpacing={4}
-
             style={{ marginTop: "40px" }}
           >
             <Grid item xs={12} md={4}>
@@ -49,7 +58,6 @@ const About = () => {
                   {i18n.t("modules.about.thirdTitle")}
                 </GStyled.Title>
                 <Typography
-
                   style={{ marginTop: "10px", whiteSpace: "pre-line" }}
                 >
                   {i18n.t("modules.about.third")}
@@ -57,12 +65,11 @@ const About = () => {
               </CardMine>
             </Grid>
             <Grid item xs={12} md={4}>
-              <CardMine duration={0.7} >
+              <CardMine duration={0.7}>
                 <GStyled.Title fontSize='16px' style={{ fontWeight: 700 }}>
                   {i18n.t("modules.about.forthTitle")}
                 </GStyled.Title>
                 <Typography
-
                   style={{ marginTop: "10px", whiteSpace: "pre-line" }}
                 >
                   {i18n.t("modules.about.forth")}
@@ -70,7 +77,6 @@ const About = () => {
               </CardMine>
             </Grid>
           </Grid>
-
         </Container>
       </Container>
       <Box
