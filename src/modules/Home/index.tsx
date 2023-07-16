7292b74daaa19b2cc8a7c9a475ae56ce768f1abe
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-16 12:46:54 +0100

    a

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -43,6 +43,7 @@ const Home = () => {
           transition={{ duration: 0.8 }}
         >
           <AbsoluteCarousel
+            automaticSlide={mobile ? undefined : 4000}
             mobile={mobile}
             height={mobile ? "150px" : "300px"}
             width={mobile ? "92vw" : "55vw"}
