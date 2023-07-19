39fa8ebbaa20831d5a1183a607425c8647743c57
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-19 12:02:00 +0100

    a

--- src/modules/Admin/ManageBooks/TestePage/index.js
+++ src/modules/Admin/ManageBooks/TestePage/index.js
@@ -1,8 +1,10 @@
-import { useMediaQuery, useTheme } from "@mui/material";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 import { motion } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import HTMLFlipBook from "react-pageflip";
 import { useSelector } from "react-redux";
+import { Colors } from "../../../../constants/pallette";
 import { useKeyPress } from "../../../../hooks/useKeyPress";
 import { i18n } from "../../../../translations/i18n";
 import "./App.css";
@@ -101,117 +103,152 @@ function MyAlbum(props) {
     }, [50]);
   };
 
-  return (
-    <div style={{ padding: "200px 0px" }}>
-      {!zoom ? (
-        <div>
-          <HTMLFlipBook
-            width={width}
-            height={height}
-            minWidth={315}
-            maxWidth={1000}
-            minHeight={420}
-            maxHeight={1350}
-            showCover={true}
-            flippingTime={1200}
-            style={{ margin: "0 auto" }}
-            maxShadowOpacity={0.5}
-            className="album-web"
-            ref={bookRef}
-            onFlip={(e) => setPage(e.data)}
-            mobileScrollSupport={true}
-          >
-            <PageCover image={book?.coverPage[0] || ""} />
-            <PageCover />
-            <Page>
-              <p>{book?.title}</p>
-              <p>{book?.author}</p>
-            </Page>
-            {listImages.map((item, index) => {
-              return (
-                <Page>
-                  <img
-                    src={item}
-                    alt=""
-                    width="100%"
-                    height="100%"
-                    objectFit="cover"
-                  />
-                </Page>
-              );
-            })}
-            {listImages.length % 2 === 0 && <Page />}
-            <PageCover></PageCover>
-            <PageCover>see you</PageCover>
-          </HTMLFlipBook>
-        </div>
-      ) : (
-        <motion.div
-          ref={constraintsRef}
-          style={{
-            height: height,
-            width: width,
-            overflow: "hidden",
-            position: "relative",
-            cursor: "grabbing",
-            placeContent: "center",
-            placeItems: "center",
-            display: "flex",
-            marginTop: mobileRotated ? "60px" : fullScreen ? "30px" : "60px",
-          }}
-        >
+  const renderBook = () => {
+    return (
+      <div style={{ padding: "200px 0px" }}>
+        {!zoom ? (
+          <div>
+            <HTMLFlipBook
+              width={width}
+              height={height}
+              minWidth={315}
+              maxWidth={1000}
+              minHeight={420}
+              maxHeight={1350}
+              showCover={true}
+              flippingTime={1200}
+              style={{ margin: "0 auto" }}
+              maxShadowOpacity={0.5}
+              className="album-web"
+              ref={bookRef}
+              onFlip={(e) => setPage(e.data)}
+              mobileScrollSupport={true}
+            >
+              <PageCover image={book?.coverPage[0] || ""} />
+              <PageCover />
+              <Page>
+                <p>{book?.title}</p>
+                <p>{book?.author}</p>
+              </Page>
+              {listImages.map((item, index) => {
+                return (
+                  <Page>
+                    <img
+                      src={item}
+                      alt=""
+                      width="100%"
+                      height="100%"
+                      objectFit="cover"
+                    />
+                  </Page>
+                );
+              })}
+              {listImages.length % 2 === 0 && <Page />}
+              <PageCover></PageCover>
+              <PageCover>see you</PageCover>
+            </HTMLFlipBook>
+          </div>
+        ) : (
           <motion.div
-            drag
-            dragConstraints={constraintsRef}
-            display="flex"
+            ref={constraintsRef}
             style={{
-              height: height * zoomRatio,
-              width: width * zoomRatio,
-              position: "absolute",
+              height: height,
+              width: width,
+              overflow: "hidden",
+              position: "relative",
+              cursor: "grabbing",
+              placeContent: "center",
+              placeItems: "center",
+              display: "flex",
+              marginTop: mobileRotated ? "60px" : fullScreen ? "30px" : "60px",
             }}
           >
-            <img
-              draggable={false}
-              src={listImages[page]}
-              width={(width / 2) * zoomRatio}
-              height={height * zoomRatio}
-              alt=""
-              style={{ objectFit: "cover" }}
-            />
-            <img
-              draggable={false}
-              alt=""
-              src={listImages[page + 1]}
-              width={(width / 2) * zoomRatio}
-              height={height * zoomRatio}
-              style={{ objectFit: "cover" }}
-            />
+            <motion.div
+              drag
+              dragConstraints={constraintsRef}
+              display="flex"
+              style={{
+                height: height * zoomRatio,
+                width: width * zoomRatio,
+                position: "absolute",
+              }}
+            >
+              <img
+                draggable={false}
+                src={listImages[page]}
+                width={(width / 2) * zoomRatio}
+                height={height * zoomRatio}
+                alt=""
+                style={{ objectFit: "cover" }}
+              />
+              <img
+                draggable={false}
+                alt=""
+                src={listImages[page + 1]}
+                width={(width / 2) * zoomRatio}
+                height={height * zoomRatio}
+                style={{ objectFit: "cover" }}
+              />
+            </motion.div>
           </motion.div>
-        </motion.div>
-      )}
-      {page !== 0 && page !== listImages.length + 5 && (
-        <div
+        )}
+        {page !== 0 && page !== listImages.length + 5 && (
+          <div
+            style={{
+              position: mobileRotated ? "inherit" : "absolute",
+              width: "70vw",
+              marginTop: mobileRotated ? "10px" : "0px",
+              left: 50,
+              bottom: 20,
+            }}
+          >
+            <p
+              style={{
+                color: "white",
+                fontSize: mobileRotated ? "18px" : "22px",
+                fontWeight: "bold",
+              }}
+            >
+              {i18n.t("modules.books.viewBook.page")} {page}-{page + 1} /{" "}
+              {listImages.length + 4}
+            </p>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {listImages.length > 1 && !mobileRotated && (
+        <Box
+          display="flex"
+          justifyContent="space-between"
           style={{
-            position: mobileRotated ? "inherit" : "absolute",
-            width: "70vw",
-            marginTop: mobileRotated ? "10px" : "0px",
-            left: 50,
-            bottom: 20,
+            position: "absolute",
+            top: "45%",
+            width: fullScreen ? "96vw" : "75vw",
+
+            zIndex: 1000,
           }}
         >
-          <p
-            style={{
-              color: "white",
-              fontSize: mobileRotated ? "18px" : "22px",
-              fontWeight: "bold",
-            }}
-          >
-            {i18n.t("modules.books.viewBook.page")} {page}-{page + 1} /{" "}
-            {listImages.length + 4}
-          </p>
-        </div>
+          <FiChevronLeft
+            size="80px"
+            color={Colors.tealc}
+            style={{ cursor: "pointer" }}
+            onClick={() => handleMove("left")}
+          />
+
+          <FiChevronRight
+            size="80px"
+            color={Colors.tealc}
+            style={{ cursor: "pointer" }}
+            onClick={() => handleMove("right")}
+          />
+        </Box>
       )}
-    </div>
+      {renderBook()}
+    </>
   );
 }
 
